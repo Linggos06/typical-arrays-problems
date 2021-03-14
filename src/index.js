a2a5ce4fa3918884a42a1cbdbdc369714dc685d8
Julia Sen <julia.snk@hotmail.com>
2021-03-14 12:44:03 +0300

    solve problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+const { bgGreen } = require("colors");
 
 exports.min = function min (array) {
-  return 0;
+  if(arguments.length === 0 || array.length === 0){
+    return 0;
+  }else {
+    array.sort((a,b) => a - b);
+    return array[0];
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(arguments.length === 0 || array.length === 0){
+    return 0;
+  }else {
+    array.sort((a,b) => a - b);
+    return array[array.length - 1];
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(arguments.length === 0 || array.length === 0){
+    return 0;
+  }else {
+   return array.reduce((acc, val) => acc += val) / array.length;
+  }
 }
